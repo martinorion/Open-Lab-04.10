9b637a7d655c01a24c26cf196eb668d7228f2aa1
martinorion <71751917+martinorion@users.noreply.github.com>
2021-01-15 18:38:40 +0100

    Return average

--- Open-Lab-04.10/Calculator.cs
+++ Open-Lab-04.10/Calculator.cs
@@ -1,12 +1,14 @@
-﻿using System;
-
+using System;
+using System.Linq;
 namespace Open_Lab_04._10
 {
     public class Calculator
     {
         public float Average(int[] nums)
         {
-            throw new NotImplementedException();
+            float sum = nums.Sum();
+            float avg = sum / nums.Length;
+            return avg;
         }
     }
 }
